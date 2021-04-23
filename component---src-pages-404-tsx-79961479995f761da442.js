744c28cd06295d682abb8d3aaf2a3f05321fdf5f
nakano0518 <m15sc018.prog@gmail.com>
2021-04-23 12:53:23 +0000

    Updates

---
+++ component---src-pages-404-tsx-79961479995f761da442.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{938:function(t,u,e){"use strict";e.r(u);e(7294);var s=e(6094),n=e(7431),a=e(3431);u.default=function(){return(0,a.tZ)(s.Z,null,(0,a.tZ)(n.Z,{title:"404: Not found"}),(0,a.tZ)("h1",null,"404: Not Found"),(0,a.tZ)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-79961479995f761da442.js.map
